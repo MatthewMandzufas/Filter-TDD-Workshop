6e96b98938b6cf112941f71e342445a20e43e19b
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-02-15 20:02:20 +0800

    Green: Given a callback function and an array › returns an array

--- src/filter.js
+++ src/filter.js
@@ -1,8 +1,10 @@
 
 function filter(callbackFunction, myArray){
+    const filteredArray = [];
     for (let i = 0; i < myArray.length; i++){
         callbackFunction(myArray[i]);
     }
+    return filteredArray;
 }
 
 export default filter
